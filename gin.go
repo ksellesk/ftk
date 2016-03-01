fdfbc1e8d419542e6c36e93e09bacf48416108ee
ksellesk <zhengdachuan200305@gmail.com>
2016-03-01 01:49:51 +0000

    add src files

---
+++ gin.go
@@ -0,0 +1,34 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	dbConn()
+	ws()
+	dbClose()
+}
+
+func ws() {
+	r := gin.Default()
+	r.LoadHTMLGlob("webroot/templates/*.html")
+	r.Static("/static", "webroot/static")
+	r.GET("/", index)
+	r.GET("/ws", servews)
+	r.Run(":80")
+}
+
+func index(c *gin.Context) {
+
+	dbSet("ksel", "xuexue")
+	title := "testing"
+	firstget := dbGet("ksel")
+	c.HTML(200, "index.html", gin.H{
+		"title": title,
+		"redis": firstget,
+	})
+
+}
+
+func servews(c *gin.Context) {
+	wshandler(c.Writer, c.Request)
+}
